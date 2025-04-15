223807d38d021b27a0ca4711f2ed5f54503fc330
Jason <b65913jl@gmail.com>
2025-04-15 14:51:15 +0900

    MT3_00_03

--- Src/MT3.h
+++ Src/MT3.h
@@ -7,6 +7,90 @@
 #include<initializer_list>
 #include<memory>
 
+#pragma warning(disable : 4201)
+
+namespace MT3 {
+	class Vec3;
+	class Mat4;
+}
+
+namespace MT3 {
+	class Vec3 {
+	public:
+		union {
+			__m128 XMM_;
+			float Entries_[4];
+			struct { float x; float y; float z; };
+		};
+
+	public:
+		inline Vec3() : XMM_{ _mm_setr_ps(0.0f, 0.0f, 0.0f, 1.0f) } {}
+		inline Vec3(__m128 xmm_) : XMM_{ xmm_ } { Entries_[3] = 1.0f; }
+		inline Vec3(float x_, float y_, float z_) : XMM_{ _mm_setr_ps(x_, y_, z_, 1.0f) } {}
+		inline Vec3(const float entries_[3]) : Vec3(entries_[0], entries_[1], entries_[2]) {}
+
+		[[nodiscard]] inline float& operator[](int i_)& { return Entries_[i_]; }
+		[[nodiscard]] inline float* operator()()& { return Entries_; }
+
+		[[nodiscard]] inline float operator[](int i_) const& { return Entries_[i_]; }
+		[[nodiscard]] inline const float* operator()() const& { return Entries_; }
+
+		friend Vec3 operator+(const Vec3& v0_, const Vec3& v1_);
+		friend Vec3 operator-(const Vec3& v0_, const Vec3& v1_);
+
+		friend Vec3 operator*(float s_, const Vec3& v_);
+		friend Vec3 operator*(const Vec3& v_, float s_);
+		friend Vec3 operator/(const Vec3& v_, float s_);
+
+		inline Vec3& operator+=(const Vec3& rhs_) {
+			XMM_ = _mm_add_ps(XMM_, rhs_.XMM_);
+			return *this;
+		}
+		inline Vec3& operator-=(const Vec3& rhs_) {
+			XMM_ = _mm_sub_ps(XMM_, rhs_.XMM_);
+			return *this;
+		}
+		inline Vec3& operator*=(float s_) {
+			XMM_ = _mm_mul_ps(XMM_, _mm_set1_ps(s_));
+			return *this;
+		}
+		inline Vec3& operator/=(float s_) {
+			XMM_ = _mm_div_ps(XMM_, _mm_set1_ps(s_));
+			return *this;
+		}
+
+		inline float Length() const {
+			return _mm_cvtss_f32(_mm_sqrt_ss(_mm_dp_ps(XMM_, XMM_, 0x71)));
+		}
+		inline Vec3 Norm() const {
+			return Vec3{ _mm_div_ps(XMM_, _mm_sqrt_ps(_mm_dp_ps(XMM_, XMM_, 0x77))) };
+		}
+
+		static inline float Dot(const Vec3& v0_, const Vec3& v1_) {
+			return _mm_cvtss_f32(_mm_dp_ps(v0_.XMM_, v1_.XMM_, 0x71));
+		}
+		static inline Vec3 Cross(const Vec3& v0_, const Vec3& v1_) {
+			return _mm_sub_ps(
+				_mm_mul_ps(
+					_mm_shuffle_ps(v0_.XMM_, v0_.XMM_, _MM_SHUFFLE(3, 0, 2, 1)),
+					_mm_shuffle_ps(v1_.XMM_, v1_.XMM_, _MM_SHUFFLE(3, 1, 0, 2))
+				),
+				_mm_mul_ps(
+					_mm_shuffle_ps(v0_.XMM_, v0_.XMM_, _MM_SHUFFLE(3, 1, 0, 2)),
+					_mm_shuffle_ps(v1_.XMM_, v1_.XMM_, _MM_SHUFFLE(3, 0, 2, 1))
+				)
+			);
+		}
+	};
+
+	inline Vec3 operator+(const Vec3& v0_, const Vec3& v1_) { return Vec3{ _mm_add_ps(v0_.XMM_, v1_.XMM_) }; }
+	inline Vec3 operator-(const Vec3& v0_, const Vec3& v1_) { return Vec3{ _mm_sub_ps(v0_.XMM_, v1_.XMM_) }; }
+
+	inline Vec3 operator*(float s_, const Vec3& v_) { return Vec3{ _mm_mul_ps(v_.XMM_, _mm_set1_ps(s_)) }; }
+	inline Vec3 operator*(const Vec3& v_, float s_) { return Vec3{ _mm_mul_ps(v_.XMM_, _mm_set1_ps(s_)) }; }
+	inline Vec3 operator/(const Vec3& v_, float s_) { return Vec3{ _mm_div_ps(v_.XMM_, _mm_set1_ps(s_)) }; }
+}
+
 inline void Swap(__m128& xmm1_, __m128& xmm2_) {
 	__m128 tmp{ xmm2_ };
 	xmm2_ = xmm1_;
@@ -47,6 +131,8 @@ namespace MT3 {
 			Multiply(*this, *this, rhs_);
 			return *this;
 		}
+		
+		friend Vec3 operator*(const Vec3& v_, const Mat4& m_);
 
 		// Implemented using row reduction
 		float Det() const {
@@ -283,5 +369,50 @@ namespace MT3 {
 				}
 			}
 		}
+
+	public:
+		static inline Mat4 MakeTranslateMatrix(const Vec3& t_) {
+			Mat4 tMat{};
+			tMat.XMMs_[3] = t_.XMM_;
+			return tMat;
+		}
+
+		static inline Mat4 MakeScaleMatrix(const Vec3& s_) {
+			return {
+				s_.x, 0.0f, 0.0f, 0.0f,
+				0.0f, s_.y, 0.0f, 0.0f,
+				0.0f, 0.0f, s_.z, 0.0f,
+				0.0f, 0.0f, 0.0f, 1.0f,
+			};
+		}
 	};
+
+	inline Vec3 operator*(const Vec3& v_, const Mat4& m_) {
+		// v01 = { v_[0], v_[0], v_[0], v_[0], v_[1], v_[1], v_[1], v_[1] }
+		__m256 v01{
+			_mm256_set_m128(
+				_mm_shuffle_ps(v_.XMM_, v_.XMM_, 0b01010101),
+				_mm_shuffle_ps(v_.XMM_, v_.XMM_, 0b00000000)
+			)
+		};
+		// v23 = { v_[2], v_[2], v_[2], v_[2], v_[3], v_[3], v_[3], v_[3] }
+		__m256 v23{
+			_mm256_set_m128(
+				_mm_shuffle_ps(v_.XMM_, v_.XMM_, 0b11111111),
+				_mm_shuffle_ps(v_.XMM_, v_.XMM_, 0b10101010)
+			)
+		};
+		// _mm256_mul_ps(v01, m_.YMMs_[0]) =
+		// { v_[0] * m_[0][0], v_[0] * m_[0][1], v_[0] * m_[0][2], v_[0] * m_[0][3],
+		//   v_[1] * m_[1][0], v_[1] * m_[1][1], v_[1] * m_[1][2], v_[1] * m_[1][3] }
+		// _mm256_mul_ps(v23, m_.YMMs_[1]) =
+		// { v_[2] * m_[2][0], v_[2] * m_[2][1], v_[2] * m_[2][2], v_[2] * m_[2][3],
+		//   v_[3] * m_[3][0], v_[3] * m_[3][1], v_[3] * m_[3][2], v_[3] * m_[3][3] }
+		__m256 result256{ _mm256_add_ps(_mm256_mul_ps(v01, m_.YMMs_[0]), _mm256_mul_ps(v23, m_.YMMs_[1])) };
+		__m128 result128{ _mm_add_ps(_mm256_castps256_ps128(result256), _mm256_extractf128_ps(result256, 1)) };
+		float n[4]{};
+		_mm_store_ps(n, result128);
+		n[3] = 1.0f / n[3];
+		return Vec3{ n[0] * n[3], n[1] * n[3], n[2] * n[3] };
+	}
 }
--- main.cpp
+++ main.cpp
@@ -1,16 +1,67 @@
 #include <Novice.h>
 
-#include"HW_00/HW_00_02.h"
+#include"Src/MT3.h"
+#include<format>
 
-const char kWindowTitle[] = "LE2C_08_コウ_シキン_MT3_00_02";
+const char kWindowTitle[] = "LE2C_08_コウ_シキン_MT3_00_03";
+
+namespace MT3 {
+	namespace {
+		void PrintMat4(const Mat4& m_, int x_, int y_) {
+			for (int i{ 0 }; i < 4; ++i) {
+				Novice::ScreenPrintf(
+					x_, y_ + i * 20,
+					"%7.2f%7.2f%7.2f%7.2f",
+					m_[i][0], m_[i][1], m_[i][2], m_[i][3]
+				);
+			}
+		}
+	}
+
+	class HW_00_03 {
+	private:
+		Mat4 TranslateMat_{};
+		Mat4 ScaleMat_{};
+		Mat4 Transform_{
+			1.0f, 2.0f, 3.0f, 4.0f,
+			3.0f, 1.0f, 1.0f, 2.0f,
+			1.0f, 4.0f, 2.0f, 3.0f,
+			2.0f, 2.0f, 1.0f, 3.0f,
+		};
+		Vec3 TransformedPoint_{};
+
+	public:
+		HW_00_03(
+			const Vec3& translate_ = { 4.1f, 2.6f, 0.8f },
+			const Vec3& scale_ = { 1.5f, 5.2f, 7.3f },
+			const Vec3& point_ = { 2.3f, 3.8f, 1.4f }
+		) {
+			TranslateMat_ = Mat4::MakeTranslateMatrix(translate_);
+			ScaleMat_ = Mat4::MakeScaleMatrix(scale_);
+			TransformedPoint_ = point_ * Transform_;
+		}
+
+		void Print() const {
+			Novice::ScreenPrintf(
+				0, 0, "Transformed point = (%.2f, %.2f, %.2f)",
+				TransformedPoint_.x, TransformedPoint_.y, TransformedPoint_.z
+			);
+
+			Novice::ScreenPrintf(0, 40, "Translate matrix");
+			PrintMat4(TranslateMat_, 0, 60);
+			Novice::ScreenPrintf(0, 160, "Scale matrix");
+			PrintMat4(ScaleMat_, 0, 180);
+		}
+	};
+}
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
-	MT3::HW_00_02 hw_00_02{};
-
 	char keys[256]{ 0 };
 	char preKeys[256]{ 0 };
+
+	MT3::HW_00_03 hw{};
 
 	while (Novice::ProcessMessage() == 0) {
 		Novice::BeginFrame();
@@ -18,7 +69,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
-		hw_00_02.Print();
+		hw.Print();
 
 		Novice::EndFrame();
 
