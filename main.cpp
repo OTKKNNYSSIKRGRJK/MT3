3cf027369e0a0102d58a489a0716666570de014d
Yasei no Koushiki <b65913jl@gmail.com>
2025-05-23 16:35:27 +0900

    球と球の衝突判定

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 #include<ImGui.h>
 
-const char kWindowTitle[] = "LE2C_08_コウ_シキン_MT3_02_00";
+const char kWindowTitle[] = "LE2C_08_コウ_シキン_MT3_02_01";
 
 namespace MT3 {
 	namespace {
@@ -95,9 +95,9 @@ namespace MT3 {
 
 		const Mat4* VPVp_{ nullptr };
 
-		uint32_t RGBA_{ 0xFFDFAF7F };
+		uint32_t RGBA_{ 0xFFFFFF7F };
 
-		Sphere(float radius_ = 3.0f, uint32_t div1_ = 12U, uint32_t div2_ = 24U) {
+		Sphere(float radius_ = 1.0f, uint32_t div1_ = 12U, uint32_t div2_ = 24U) {
 			CreateSphereGrid(Vertices_, IndexPairs_, { 0.0f, 0.0f, 0.0f }, radius_, div1_, div2_);
 		}
 
@@ -133,32 +133,10 @@ namespace MT3 {
 		}
 	};
 
-	struct LineSegment {
-		Vec3 Origin;
-		Vec3 Diff;
-	};
-
-	inline Vec3 ProjectPointOntoLine(const Vec3& p_, const LineSegment& l_) {
-		return
-			l_.Origin +
-			(Vec3::Dot(l_.Diff, (p_ - l_.Origin)) / Vec3::Dot(l_.Diff, l_.Diff)) *
-			l_.Diff;
-	}
-	inline Vec3 FindNearestPointOnSegment(const Vec3& p_, const LineSegment& seg_) {
-		return
-			seg_.Origin +
-			std::clamp(
-				Vec3::Dot(seg_.Diff, (p_ - seg_.Origin)) / Vec3::Dot(seg_.Diff, seg_.Diff),
-				0.0f, 1.0f
-			) * seg_.Diff;
-	}
-
-	class HW_02_00 {
+	class HW_02_01 {
 	private:
-		Vec3 CameraRotate_{ 0.1f, 0.0f, 0.0f };
-		Vec3 CameraTranslate_{ 0.0f, 1.0f, -10.0f };
-		//Vec3 CameraRotate_{ 0.35f, -0.1f, 0.0f };
-		//Vec3 CameraTranslate_{ 1.0f, 4.0f, -10.0f };
+		Vec3 CameraRotate_{ 0.5f, -0.5f, 0.0f };
+		Vec3 CameraTranslate_{ 5.0f, 6.0f, -10.0f };
 
 		Mat4 Camera_{};
 
@@ -169,57 +147,47 @@ namespace MT3 {
 		//Mat4 Inv_PVp_{};
 		Mat4 VPVp_{};
 
-		LineSegment Seg_{ .Origin{ -2.0f, -1.0f, 0.0f }, .Diff{ 3.0f, 2.0f, 2.0f } };
-
-		Sphere Point_{ 0.0625f, 6U, 12U };
-		Sphere ProjectedPoint_{ 0.0625f, 6U, 12U };
-		Sphere NearestPoint_{ 0.0625f, 6U, 12U };
+		std::vector<Sphere> Spheres_{};
+		std::vector<float> Radii_{};
 
 		Grid Grid_{};
 
 		char Keys_[256]{};
 
+		bool IsCollided(const Sphere& s0_, const Sphere& s1_) {
+			auto&& p01{ s1_.Translate_ - s0_.Translate_ };
+			float sq_Dist = Vec3::Dot(p01, p01);
+			float sq_Sum_Raii = (s0_.Scale_.x + s1_.Scale_.x) * (s0_.Scale_.x + s1_.Scale_.x);
+			#if defined(_DEBUG)
+			ImGui::Begin("MT3");
+			{
+				ImGui::SeparatorText("Collision");
+				ImGui::Text("Distance^2 = %f", sq_Dist);
+				ImGui::Text("(Sum of Radii)^2 = %f", sq_Sum_Raii);
+			}
+			ImGui::End();
+			#endif
+			return (sq_Dist <= sq_Sum_Raii);
+		}
+
 	public:
-		HW_02_00() {
+		HW_02_01() {
 			Mat4::Multiply(PVp_, Projection_, Viewport_);
 			//Mat4::Invert(Inv_PVp_, PVp_);
 
-			Point_.VPVp_ = &VPVp_;
-			ProjectedPoint_.VPVp_ = &VPVp_;
-			NearestPoint_.VPVp_ = &VPVp_;
 			Grid_.VPVp_ = &VPVp_;
 
-			Point_.Translate_ = { -1.5f, 0.6f, 0.6f };
-			Point_.RGBA_ = 0xFFDF7FFF;
-			ProjectedPoint_.RGBA_ = 0xFFDF7F3F;
-			NearestPoint_.RGBA_ = 0xFFFFFFFF;
-
-			/*Mat4 camera{ CameraRotateMat_ };
-			camera[3][0] = CameraTranslate_.x;
-			camera[3][1] = CameraTranslate_.y;
-			camera[3][2] = CameraTranslate_.z;
-			Mat4::Invert(View_, camera);*/
+			auto& sphere0 = Spheres_.emplace_back();
+			sphere0.VPVp_ = &VPVp_;
+			sphere0.Translate_ = { 1.0f, 0.0f, 3.0f };
+			auto& sphere1 = Spheres_.emplace_back();
+			sphere1.VPVp_ = &VPVp_;
+			sphere1.Translate_ = { -1.0f, 0.5f, -1.0f };
+			Radii_.emplace_back(1.0f);
+			Radii_.emplace_back(0.5f);
 		}
 
 		void Update() {
-			/*Novice::GetHitKeyStateAll(Keys_);
-			Vec3 tmpT{}, tmpR{};
-			if (Keys_[DIK_UP]) { tmpT.z = -0.05f; }
-			if (Keys_[DIK_DOWN]) { tmpT.z = 0.05f; }
-			if (Keys_[DIK_LEFT]) { tmpT.x = 0.05f; }
-			if (Keys_[DIK_RIGHT]) { tmpT.x = -0.05f; }
-			if (Keys_[DIK_W]) { tmpR.x = -0.005f; }
-			if (Keys_[DIK_S]) { tmpR.x = 0.005f; }
-			if (Keys_[DIK_A]) { tmpR.y = -0.005f; }
-			if (Keys_[DIK_D]) { tmpR.y = 0.005f; }
-			
-			Mat4&& rotate{ Mat4::MakeRotateMatrix(tmpR) };
-			Mat4 inv_Rotate{};
-			Mat4::Invert(inv_Rotate, rotate);
-			Mat4::Multiply(View_, View_, inv_Rotate);
-			View_[3][0] += tmpT.x;
-			View_[3][2] += tmpT.z;*/
-
 			ImGui::Begin("MT3");
 			{
 				ImGui::SeparatorText("Camera");
@@ -238,53 +206,35 @@ namespace MT3 {
 
 			#if defined(_DEBUG)
 			ImGui::Begin("MT3");
-			{
-				ImGui::SeparatorText("Projection of point on segment");
+			for (int i = 0; i < static_cast<int>(Spheres_.size()); ++i) {
+				ImGui::SeparatorText(std::format("Sphere #{}", i).data());
 				{
-					ImGui::BulletText("Segment");
-					ImGui::DragFloat3("Origin", Seg_.Origin(), 0.01f);
-					ImGui::DragFloat3("Diff", Seg_.Diff(), 0.01f);
-					ImGui::BulletText("Point");
-					ImGui::DragFloat3("Pos", Point_.Translate_(), 0.01f);
-					ImGui::InputFloat3("Projected Pos", ProjectedPoint_.Translate_(), "%.3f", ImGuiInputTextFlags_ReadOnly);
-					ImGui::InputFloat3("Nearest Pos", NearestPoint_.Translate_(), "%.3f", ImGuiInputTextFlags_ReadOnly);
+					ImGui::DragFloat3(std::format("Center##Sphere#{}", i).data(), Spheres_[i].Translate_(), 0.01f);
+					ImGui::DragFloat(std::format("Radius##Sphere#{}", i).data(), &Radii_[i], 0.01f);
 				}
 			}
 			ImGui::End();
 			#endif
 
-			Point_.Update();
+			for (int i = 0; i < static_cast<int>(Spheres_.size()); ++i) {
+				Spheres_[i].Scale_ = { Radii_[i], Radii_[i], Radii_[i] };
+				Spheres_[i].Update();
+			}
 
-			ProjectedPoint_.Translate_ = ProjectPointOntoLine(Point_.Translate_, Seg_);
-			ProjectedPoint_.Update();
-			NearestPoint_.Translate_ = FindNearestPointOnSegment(Point_.Translate_, Seg_);
-			NearestPoint_.Update();
+			if (IsCollided(Spheres_[0], Spheres_[1])) {
+				Spheres_[1].RGBA_ = 0xDF1F2F7F;
+			}
+			else {
+				Spheres_[1].RGBA_ = 0xFFFFFF7F;
+			}
 		}
 
 		void Draw() {
 			Grid_.Draw();
-			Point_.Draw();
 
-			ProjectedPoint_.Draw();
-			Vec3 st{ Seg_.Origin * VPVp_ };
-			Vec3 ed{ ProjectedPoint_.Translate_ * VPVp_ };
-			Novice::DrawLine(
-				static_cast<int>(st.x),
-				static_cast<int>(st.y),
-				static_cast<int>(ed.x),
-				static_cast<int>(ed.y),
-				0xFFDF7F3F
-			);
-
-			NearestPoint_.Draw();
-			ed = (Seg_.Origin + Seg_.Diff) * VPVp_;
-			Novice::DrawLine(
-				static_cast<int>(st.x),
-				static_cast<int>(st.y),
-				static_cast<int>(ed.x),
-				static_cast<int>(ed.y),
-				0xFFFFFFFF
-			);
+			for (int i = 0; i < static_cast<int>(Spheres_.size()); ++i) {
+				Spheres_[i].Draw();
+			}
 		}
 	};
 }
@@ -295,7 +245,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256]{ 0 };
 	char preKeys[256]{ 0 };
 
-	MT3::HW_02_00 hw{};
+	MT3::HW_02_01 hw{};
 
 	while (Novice::ProcessMessage() == 0) {
 		Novice::BeginFrame();
